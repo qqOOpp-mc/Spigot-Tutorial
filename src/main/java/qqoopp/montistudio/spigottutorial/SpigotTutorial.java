a1902a463ef6a847282501f50214fd17198b72ef
unknown <qqoopp.mc@gmail.com>
2022-07-14 08:40:38 +0900

    Update message output when the server starts and closes

--- src/main/java/qqoopp/montistudio/spigottutorial/SpigotTutorial.java
+++ src/main/java/qqoopp/montistudio/spigottutorial/SpigotTutorial.java
@@ -4,14 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SpigotTutorial extends JavaPlugin {
 
+    /* Runs when plugin is loaded on the server
+     * (When the server is starting)
+     */
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        System.out.print("Spigot Tutorial Plugin is on enabled!");
     }
 
+    /* Runs when plugin is unloaded on the server
+     * (When the server is closing)
+     */
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        System.out.print("Spigot Tutorial Plugin is on enabled!");
     }
 }
