0eb296c15e628f93fe68d65caf2b34cb72fb2192
unknown <qqoopp.mc@gmail.com>
2022-07-14 08:28:54 +0900

    Create new 1.16.5 project

---
+++ src/main/java/qqoopp/montistudio/spigottutorial/SpigotTutorial.java
@@ -0,0 +1,17 @@
+package qqoopp.montistudio.spigottutorial;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpigotTutorial extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
